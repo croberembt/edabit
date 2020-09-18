858f4695e4b6555082c612938c12896ce9a5fd4d
Calli Oberembt <croberembt@gmail.com>
2020-09-18 16:51:44 -0500

    return the sum of two numbers

---
+++ return-the-sum-of-two-numbers.js
@@ -0,0 +1,15 @@
+/*
+
+Instructions: Create a function that takes two numbers as arguments and return their sum.
+
+Code below: 
+
+*/
+
+function addition(a, b) {
+    return a + b; 
+} 
+
+or
+
+addition = (a, b) => a + b; 
